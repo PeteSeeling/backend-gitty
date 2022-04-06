670deeea54e2f80a634a97fa90e8088cb663be70
Pete Seeling <peteseeling@gmail.com>
2022-04-06 17:49:31 -0600

    sql changed to reduce table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,10 +1,8 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS github_users;
+DROP TABLE IF EXISTS github_users CASCADE;
 
 CREATE TABLE github_users (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    username TEXT NOT NULL,
-    email TEXT,
-    avatar TEXT
+    github_username TEXT NOT NULL PRIMARY KEY,
+    github_avatar TEXT NOT NULL
 )
