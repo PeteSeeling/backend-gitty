a843b2782ebcca69ef46c13dd6088ee053383942
Pete Seeling <peteseeling@gmail.com>
2022-04-08 13:17:30 -0600

    test file cleaned up

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,6 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const GithubUser = require('../lib/models/User');
 
 jest.mock('../lib/utils/github');
 
@@ -19,7 +18,6 @@ describe('backend-gitty routes', () => {
     const req = await request(app).get('/api/v1/github/login');
 
     expect(req.header.location).toMatch(/https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i);
-
   });
 
   it('should login user and redirect user to dashboard', async () => {
